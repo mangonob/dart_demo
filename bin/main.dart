d284582b8fe8d6d94f31edd68122b611d5f4e07d
高炼 <mangonob@qq.com>
2021-07-09 18:26:09 +0800

    feat: Bicycle

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,26 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  var bike = Bicycle(2, 1);
+
+  bike
+    ..speedUp(30)
+    ..applyBreak(10)
+    ..applyBreak(20);
+
+  print(bike);
+}
+
+class Bicycle {
+  int cadence;
+  int _speed = 0;
+  int get speed => _speed;
+  int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBreak(int decrement) => _speed -= decrement;
+
+  void speedUp(int increment) => _speed += increment;
+
+  @override
+  String toString() => 'Bicycle: $speed mph';
 }
